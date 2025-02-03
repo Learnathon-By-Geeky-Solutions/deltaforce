e47c16949ec72af8f8fa44d8d74ffa151014419f
sbfrusho <rushocseru28@gmail.com>
2025-02-04 00:59:45 +0600

    Authentication UI design
    1. A splash screen has been created
    2. Sign up screen has been created
    3. Login Screen has been created
    4. Successful Sign up process using the UI
    5. custom widgets(button, snackbar)

---
+++ voice_bridge/lib/views/auth/login_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:voice_bridge/view_models/auth_view_model.dart';
+import 'package:voice_bridge/widgets/custom_button.dart';
+import 'signup_screen.dart';
+
+class LoginScreen extends StatelessWidget {
+  final AuthViewModel _authController = Get.put(AuthViewModel());
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Login")),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            TextField(
+              controller: emailController,
+              decoration: InputDecoration(labelText: "Email"),
+            ),
+            SizedBox(height: 10),
+            TextField(
+              controller: passwordController,
+              decoration: InputDecoration(labelText: "Password"),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            CustomButton(
+              text: "Login",
+              onPressed: () async {
+                await _authController.singIn(emailController.text, passwordController.text);
+              },
+            ),
+            TextButton(
+              onPressed: () => Get.to(() => SignupScreen()),
+              child: Text("Don't have an account? Sign up"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ voice_bridge/lib/views/auth/signup_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:voice_bridge/view_models/auth_view_model.dart';
+import 'package:voice_bridge/widgets/custom_button.dart';
+
+class SignupScreen extends StatelessWidget {
+  final AuthViewModel _authController = Get.find<AuthViewModel>();
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Sign Up")),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            TextField(
+              controller: emailController,
+              decoration: InputDecoration(labelText: "Email"),
+            ),
+            SizedBox(height: 10),
+            TextField(
+              controller: passwordController,
+              decoration: InputDecoration(labelText: "Password"),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            CustomButton(
+              text: "Sign Up",
+              onPressed: () async {
+                await _authController.signUp(emailController.text, passwordController.text);
+              },
+            ),
+            TextButton(
+              onPressed: () => Get.back(),
+              child: Text("Already have an account? Login"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
