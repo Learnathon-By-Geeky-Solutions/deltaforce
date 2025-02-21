34a2dfbbac9bf33958e940348446420a1b48aa39
Uttam-Sarkar <81841458+Uttam-Sarkar@users.noreply.github.com>
2025-02-22 01:13:03 +0600

    delete unnecessary item

--- voice_bridge/lib/core/theme/app_colors.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const Color primary = Color(0xFF89CFF0);   // Soft Blue (Calming & Soothing)
-  static const Color secondary = Color(0xFF98FB98); // Pastel Green (Comforting for autistic children,)
-  static const Color background = Color(0xFFF5F5F5); // Light Gray (Neutral)
-  static const Color textPrimary = Color(0xFF212121); // Dark Gray (Good Readability)
-  static const Color textSecondary = Color(0xFF757575); // Light Gray
-}
--- voice_bridge/lib/main.dart
+++ voice_bridge/lib/main.dart
@@ -3,6 +3,9 @@ import 'package:get/get.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:voice_bridge/features/authentication/const/app_strings.dart';
 import 'package:voice_bridge/features/authentication/views/auth/splash_screen.dart';
+import 'package:voice_bridge/resources/colors/app_color.dart';
+import 'package:voice_bridge/resources/getx_localization/languages.dart';
+import 'package:voice_bridge/resources/routes/routes.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(); // Ensure Firebase is initialized before running the app
@@ -10,12 +13,24 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: AppStrings.appName,
-      theme: ThemeData(primarySwatch: Colors.blue),
+      // title: AppStrings.appName,
+      // theme: ThemeData(primarySwatch: Colors.blue),
+      title: 'Flutter Demo',
+      translations: Languages(),
+      locale: Locale('en' , 'US'),
+      fallbackLocale: Locale('en' , 'US'),// if locale language is not supported for the device
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: AppColor.appBarColor),
+        useMaterial3: true,
+      ),
+
+      // getPages: AppRoutes.appRoutes(),
       home: SplashScreen(),
     );
   }
