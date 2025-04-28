597c7478844fb989105aef1751445dee1238bb95
sfbrusho <rushocseru28@gmail.com>
2025-04-28 10:57:46 +0600

    update

--- voice_bridge/lib/features/authentication/views/auth/splash_screen.dart
+++ voice_bridge/lib/features/authentication/views/auth/splash_screen.dart
@@ -2,13 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:voice_bridge/features/authentication/const/app_strings.dart';
 import 'package:voice_bridge/features/authentication/view_models/auth_view_model.dart';
-import 'package:voice_bridge/resources/routes/routesName.dart';
 
 class SplashScreen extends StatelessWidget {
   final AuthViewModel _authViewModel = Get.find<AuthViewModel>();
 
   SplashScreen({super.key}) {
-    Future.delayed(Duration(seconds: 2), () {
+    Future.delayed(const Duration(seconds: 2), () {
       _authViewModel.checkUserLoggedIn();
     });
   }
@@ -16,16 +15,44 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueAccent,
-      body: Center(
-        child: Text(
-          AppStrings.appName,
-          style: TextStyle(
-            fontSize: 28,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
+      backgroundColor: Colors.white70,
+      body: Stack(
+        children: [
+          Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                // App Logo
+                Image.asset(
+                  'assets/appLogo.png',
+                  height: 120,
+                ),
+                const SizedBox(height: 20),
+                // App Name
+                Text(
+                  AppStrings.appName,
+                  style: const TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    letterSpacing: 1.5,
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
+          // Loading Indicator at Bottom
+          Positioned(
+            bottom: 50,
+            left: 0,
+            right: 0,
+            child: Center(
+              child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
