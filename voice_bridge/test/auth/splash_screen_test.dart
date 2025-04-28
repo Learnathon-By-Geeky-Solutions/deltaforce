ec9118a627d379f561cbd635a7f57ffc6ef7c213
Uttam-Sarkar <81841458+Uttam-Sarkar@users.noreply.github.com>
2025-04-29 02:25:45 +0600

    update splash_screen_test.dart

--- voice_bridge/test/auth/splash_screen_test.dart
+++ voice_bridge/test/auth/splash_screen_test.dart
@@ -1,36 +1,43 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
-import 'package:mockito/mockito.dart';
 import 'package:voice_bridge/features/authentication/const/app_strings.dart';
 import 'package:voice_bridge/features/authentication/view_models/auth_view_model.dart';
 import 'package:voice_bridge/features/authentication/views/auth/splash_screen.dart';
 
-// Mock AuthViewModel
-class MockAuthViewModel extends GetxService with Mock implements AuthViewModel {}
+// Fake AuthViewModel
+class FakeAuthViewModel extends GetxService implements AuthViewModel {
+  bool wasCalled = false;
+
+  @override
+  void checkUserLoggedIn() {
+    wasCalled = true;
+  }
+
+  @override
+  noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
+}
+
 
 void main() {
-  late MockAuthViewModel mockAuthViewModel;
+  late FakeAuthViewModel fakeAuthViewModel;
 
   setUp(() {
-    mockAuthViewModel = MockAuthViewModel();
-    Get.put<AuthViewModel>(mockAuthViewModel); // Inject into GetX
+    Get.reset();
+    fakeAuthViewModel = FakeAuthViewModel();
+    Get.put<AuthViewModel>(fakeAuthViewModel);
   });
 
   testWidgets('SplashScreen shows app name and calls checkUserLoggedIn after delay', (WidgetTester tester) async {
-    // Act
     await tester.pumpWidget(
       GetMaterialApp(
         home: SplashScreen(),
       ),
     );
 
-    // Assert UI immediately
     expect(find.text(AppStrings.appName), findsOneWidget);
 
-    // Fast-forward 2 seconds
-    await tester.pump(const Duration(seconds: 2));
+    await tester.pump(const Duration(seconds: 3));
 
-    // Verify that checkUserLoggedIn is called
-    verify(mockAuthViewModel.checkUserLoggedIn()).called(1);
+    expect(fakeAuthViewModel.wasCalled, isTrue);
   });
 }
